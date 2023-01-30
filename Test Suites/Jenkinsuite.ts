39efa3a43cfe79e9bbb40ecdcba5c6a8675668aa
MohammedSaleem <MohammedSaleem@DESKTOP-M5790RI>
2023-01-30 11:35:48 +0530

    New test suite "jenkinsuite"

---
+++ Test Suites/Jenkinsuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jenkinsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a15ef979-46e0-4cd0-b010-5e4d486f0c60</testSuiteGuid>
+</TestSuiteEntity>
